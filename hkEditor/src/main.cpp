0b6b65060b6143cf1f0d2a6897fe32793e4cff6a
Max Maldonado <nuup20@gmail.com>
2023-02-08 11:07:14 -0600

    add: display FPS

--- hkEditor/src/main.cpp
+++ hkEditor/src/main.cpp
@@ -40,8 +40,7 @@ int main(int argc, char* argv[])
   engineConfig.windowConfiguration.height = 800;
   engineConfig.windowConfiguration.title = "Hakool Editor";
 
-  Logger::Prepare(new LoggerConsole());
-  
+  Logger::Prepare(new LoggerConsole());  
   if (pEngine->init(engineConfig, &(Logger::GetReference())) != hk::eRESULT::kSuccess)
   {
     Hakool::Shutdown();
@@ -83,27 +82,13 @@ int main(int argc, char* argv[])
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-    {
-      static float f = 0.0f;
-      static int counter = 0;
-
-      
-      ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-      ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-      if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
-
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-      ImGui::End();
-    }
-
+    ImGui::Begin("Engine Status");    
+    ImGui::Text("FPS %.1f", ImGui::GetIO().Framerate);
+    ImGui::End();
+    
     float dt = pEngine->getDeltaTime().asSeconds();
     time += dt;
+
     float x = hk::Math::Sin(time) * 3.0f;
     float y = hk::Math::Cos(time) * 3.0f;
 
